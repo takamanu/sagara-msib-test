c6c2bb30486bcb30bfe802d75b445206cb6d7680
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2024-08-19 23:30:58 +0700

    ADD: repository baju

---
+++ src/repositories/BajuRepository.js
@@ -0,0 +1,46 @@
+const Baju = require("../models/Baju");
+
+class BajuRepository {
+  async findAll() {
+    const query = "SELECT * FROM baju";
+    const result = await this.pool.query(query);
+    return result.rows.map(
+      (row) => new Baju(row.id, row.warna, row.ukuran, row.harga, row.stok)
+    );
+  }
+
+  async create(baju) {
+    const { warna, ukuran, harga, stok } = baju;
+    const query =
+      "INSERT INTO baju (warna, ukuran, harga, stok) VALUES ($1, $2, $3, $4) RETURNING *";
+    const values = [warna, ukuran, harga, stok];
+    const result = await this.pool.query(query, values);
+    return new Baju(result.rows[0].id, warna, ukuran, harga, stok);
+  }
+
+  async findById(id) {
+    const query = "SELECT * FROM baju WHERE id = $1";
+    const result = await this.pool.query(query, [id]);
+    if (result.rows.length === 0) return null;
+    const row = result.rows[0];
+    return new Baju(row.id, row.warna, row.ukuran, row.harga, row.stok);
+  }
+
+  async update(id, dataBaju) {
+    const { warna, ukuran, harga, stok } = dataBaju;
+    const query =
+      "UPDATE baju SET warna = $1, ukuran = $2, harga = $3, stok = $4 WHERE id = $5 RETURNING *";
+    const values = [warna, ukuran, harga, stok, id];
+    const result = await this.pool.query(query, values);
+    if (result.rows.length === 0) return null;
+    return new Baju(id, warna, ukuran, harga, stok);
+  }
+
+  async delete(id) {
+    const query = "DELETE FROM baju WHERE id = $1 RETURNING *";
+    const result = await this.pool.query(query, [id]);
+    return result.rowCount > 0;
+  }
+}
+
+module.exports = BajuRepository;
